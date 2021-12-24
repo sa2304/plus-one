d90a3b3c9acd0ead8f93a1e30d83d977c5b679cb
Антон Шмидт <schmidt.ajax@gmail.com>
2021-12-24 21:05:00 +0700

    Ыщдгешщт

--- main.cpp
+++ main.cpp
@@ -7,7 +7,21 @@ using namespace std;
 class Solution {
  public:
   vector<int> plusOne(vector<int> &digits) {
-    // FIXME
+    if (not digits.empty()) {
+      ++digits.back();
+      for (int i = digits.size() - 1; 0 < i; --i) {
+        if (9 < digits[i]) {
+          digits[i] %= 10;
+          ++digits[i - 1];
+        } else { break; }
+      }
+
+      if (9 < digits.front()) {
+        digits[0] %= 10;
+        digits.insert(digits.begin(), 1);
+      }
+    }
+
     return digits;
   }
 };
@@ -25,6 +39,10 @@ void TestPlusOne() {
   {
     vector<int> digits{9};
     assert((vector<int>{1, 0}) == s.plusOne(digits));
+  }
+  {
+    vector<int> digits{1,2,9,9};
+    assert((vector<int>{1,3,0,0}) == s.plusOne(digits));
   }
 }
 
